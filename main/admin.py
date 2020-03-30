ed16152801c05be28b3d9ee84825d6d07c14ad68
pdmcguckian <59895161+pdmcguckian@users.noreply.github.com>
2020-03-30 13:52:15 +0100

    MySQL Working

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import StructuredProject, PersonalProject, StructuredProjectContent, StructuredProjectCode
+from .models import PersonalProject, StructuredProjectContent, StructuredProjectCode, StructuredProject
 # Register your models here.
 
 class StructuredProjectAdmin(admin.ModelAdmin):
