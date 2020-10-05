a7e51105674100263e0fb532eed063312fee2c2f
ocolebourne <ocolebourne@users.noreply.github.com>
2020-10-05 18:11:06 +0100

    change statics folder

--- webapp/settings.py
+++ webapp/settings.py
@@ -79,6 +79,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'webapp.wsgi.application'
 
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Database
@@ -147,13 +148,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
+    os.path.join(BASE_DIR, "static"),
 ]
-
 #django_heroku.settings(locals())
 
